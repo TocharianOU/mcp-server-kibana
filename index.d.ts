09e728a15bf42bc454b443dd62175a7e1378f11e
TocharinOU <tocharian139@protonmail.com>
2025-05-21 00:15:31 +0800

    fix: tsconfig for build

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { ServerCreationOptions } from "./src/types.js";
+export declare function createKibanaMcpServer(options: ServerCreationOptions): Promise<McpServer>;
